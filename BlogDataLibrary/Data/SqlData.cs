a807fe5bec03411bd2b9e4303a1cc824283bfc67
jcveroy <jamescarlosveroy@gmail.com>
2025-08-28 20:32:28 +0800

    Add project files.

---
+++ BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogDataLibrary.Database;
+
+namespace BlogDataLibrary.Data
+{
+    internal class SqlData
+    {
+        private ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ VeroyBlogDB/dbo/Tables/Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Username]  NVARCHAR(16)  NOT NULL,
+    [FirstName]  NVARCHAR(50) NOT NULL,
+    [LastName] NVARCHAR(50)  NOT NULL,
+    [Password]  NVARCHAR(16)  NOT NULL,
+)
+
+
